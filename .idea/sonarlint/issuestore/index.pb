1f06a8846fcd917948a9dc0817c58c0fa7df1da0
Mateus Alves <mateus.silva@midway.com.br>
2022-06-25 09:50:08 -0300

    initial commit

---
+++ .idea/sonarlint/issuestore/index.pb
@@ -0,0 +1,3 @@
+
+J
+Mango.Web/Mango.Web.csproj,a/0/a0f4accb4685663821407b2ea81afb5f8538f890
---
+++ Mango.Services.ProductAPI/DbContexts/ApplicationDbContext.cs
@@ -0,0 +1,13 @@
+using Mango.Services.ProductAPI.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Mango.Services.ProductAPI.DbContexts;
+
+public class ApplicationDbContext: DbContext
+{
+    public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options): base(options)
+    {
+        
+    }
+    public DbSet<Product> Products { get; set; }
+}
